05d1aef354c62e1c48d1852bf5e4211e648e269e
Desmond Stular <dstular@yahoo.com>
2025-02-03 23:12:12 -0700

    Added caret blank template.

---
+++ src/components/terminal/caret/Caret.tsx
@@ -0,0 +1,17 @@
+/**
+ * Caret.tsx
+ *
+ * This React component represents the text field which the user enters into.
+ * Has a callback that returns input to history.
+ *
+ * Author: Desmond Stular
+ * Date: February 3, 2024
+ */
+
+const Caret = () => {
+  return (
+    <></>
+  )
+}
+
+export default Caret;
