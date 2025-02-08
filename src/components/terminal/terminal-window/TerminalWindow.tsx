227620c0b748e75b51143523d1c39ff28029c50e
Desmond Stular <dstular@yahoo.com>
2025-02-08 14:33:32 -0700

    Terminal prevents users from selecting user identifier and deleting. Added copy command as well

--- src/components/terminal/terminal-window/TerminalWindow.tsx
+++ src/components/terminal/terminal-window/TerminalWindow.tsx
@@ -16,33 +16,58 @@ const TerminalWindow = () => {
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (inputRef.current) {
-      if (e.key === "Enter") {
-        const inputValue = inputRef.current.value.split(userIdentifier)[1];
+      const cursorPos: number | null = inputRef.current.selectionStart;
+      const selectEnd: number | null = inputRef.current.selectionEnd;
+      const inputValue: string = inputRef.current.value.length === userIdentifier.length ? '' : inputRef.current.value.split(userIdentifier)[1];
 
-        console.log(inputValue)
+      // If cursor position is not null
+      if (cursorPos !== null && selectEnd !== null) {
 
-        if (inputValue === "clear") {
-          setHistory([]);
-        } else {
-          setHistory([...history, inputValue]);
+        // If user hits enter key to input command
+        if (e.key === "Enter") {
+
+          // Clear history command
+          if (inputValue === "clear") {
+            setHistory([]);
+          } else {
+            setHistory([...history, inputValue]);
+          }
+
+          inputRef.current.value = userIdentifier;
+          inputRef.current.scrollIntoView({behavior: "smooth"})
+          e.preventDefault()
         }
-        inputRef.current.value = userIdentifier;
-        inputRef.current.scrollIntoView({behavior: "smooth"})
-        e.preventDefault()
-      }
 
-      else if (e.ctrlKey && e.key === "c") {
-        const inputValue = inputRef.current.value;
-        console.log(inputValue);
-        inputRef.current.value = '';
-        setHistory([...history, "^C"]);
-        inputRef.current.scrollIntoView({behavior: "smooth"});
-        e.preventDefault();
-      }
+        // if user enters ctrl key and c to end input line
+        else if (e.ctrlKey && e.key === "c") {
+          // If trying to copy text, copy and reset caret to selection end
+          if (cursorPos !== selectEnd) {
+            setTimeout(() => {
+              if (inputRef.current !== null) {
+                inputRef.current.selectionStart = inputRef.current.selectionEnd;
+              }
+            }, 0);
+          }
 
-      else if (e.key === "Backspace") {
-        if (inputRef.current.value === userIdentifier) {
-          e.preventDefault();
+          // If trying to send ctrl+c stop command
+          else {
+            inputRef.current.value = '';
+            setHistory([...history, `${inputValue}^C`]);
+            inputRef.current.scrollIntoView({behavior: "smooth"});
+            e.preventDefault();
+          }
+        }
+
+        // Ensure user cannot delete user identifier
+        else if (e.key === "Backspace" || e.key === "Delete") {
+          if (cursorPos <= userIdentifier.length) {
+            if (selectEnd <= userIdentifier.length) {
+              e.preventDefault();
+            }
+
+            // Reset cursor to start of input field
+            inputRef.current.selectionStart = userIdentifier.length;
+          }
         }
       }
     }
@@ -73,12 +98,13 @@ const TerminalWindow = () => {
         <p className="terminal-history-line">{userIdentifier} {history}</p>
       ))}
       <div className="terminal-input">
-        <input
+        <textarea
           className="terminal-input-field"
           spellCheck="false"
           ref={inputRef}
           onKeyDown={onKeyDown}>
-        </input>
+
+        </textarea>
       </div>
     </div>
   )
